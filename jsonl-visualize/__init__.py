bf544e037601e8ff2795fc970cd68e4ac438681e
gr-b <griffin.bishop24@gmail.com>
2024-09-30 11:06:18 -0400

    Add packaging materials

---
+++ jsonl-visualize/__init__.py
@@ -0,0 +1,1 @@
+from .jsonl-visualize import main
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='jsonl-visualize',
+    version='0.1.0',
+    description="A TUI application to visually inspect and navigate JSON and JSONL data",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[
+        'textual',
+    ],
+    entry_points={
+        'console_scripts': [
+            'jsonl-visualize=jsonl_visualize.main:main',
+        ],
+    },
+    python_requires='>=3.7',
+    license='MIT',
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/gr-b/jsonl-visualize",
+)
